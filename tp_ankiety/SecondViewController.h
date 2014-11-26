707ad4051985d36d30d8c2808bbf7db633a636fa
Aleksander Grzyb <aleksandergrzyb@Aleksanders-MacBook-Pro.local>
2014-11-26 11:45:19 +0100

    Initial Commit

---
+++ tp_ankiety/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  tp_ankiety
+//
+//  Created by Aleksander Grzyb on 26/11/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ tp_ankiety/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tp_ankiety
+//
+//  Created by Aleksander Grzyb on 26/11/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
